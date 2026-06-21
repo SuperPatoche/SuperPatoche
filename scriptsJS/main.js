5f9c7a4b21117fbd05d5921f1e0636240509a0c0
Pat PoFrancusku <153401382+SuperPatoche@users.noreply.github.com>
2026-06-21 10:43:31 +0200

    Add error logging with styled console output

--- scriptsJS/main.js
+++ scriptsJS/main.js
@@ -1,3 +1,13 @@
 console.log("fsdfsdfsdfsd\nsdfsd");
 console.log(Window.name.toString());
 console.log("prout");
+
+process.stdout.write("sdfdsf");
+
+
+console.log(
+  "%cError:%c Something went wrong! %c[Details]",
+  "color: red; font-weight: bold;",
+  "color: orange;",
+  "color: gray; font-size: 0.8em;"
+);
